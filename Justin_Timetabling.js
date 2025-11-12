9e63eb9c6b399381f6af4ab8b02191b10b117b15
JustinBlood <98722687+JustinBlood@users.noreply.github.com>
2025-11-12 08:33:35 +0800

    finish

--- Justin_Timetabling.js
+++ Justin_Timetabling.js
@@ -1,28 +1,52 @@
 module.exports = {
-    timetable:[],
+    timetable: [],
     // This allow user to enter the details of the scheduled classes
-    scheduleClass(module,day,location,hours) {
+    scheduleClass(module, day, location, start, end, hours) {
         this.timetable.push({
             module: module,
             day: day,
             location: location,
+            start: start,
+            end, end,
             hours: hours
         });
     },
-    // Explain what function B does
+    // This finds the module name and adds tutorName into the specific object
     addTutor(modName, tutorName) {
-        
+        const result = this.timetable.filter(({ module }) => module === modName);
+        result.forEach(timetable => timetable.tutor = tutorName)
     },
-    // Explain what function C does
-    updateTimetable() {
-        console.log("Hello function B");
+    // This allows you to edit the specific module
+    updateTimetable(index, newModule, newDay, newLocation, newStart, newEnd, newHrs) {
+        result = this.timetable[index];
+        if (newModule != '') {
+            result.module = newModule;
+        }
+        if (newDay != '') {
+            result.day = newDay;
+        }
+        if (newLocation != '') {
+            result.location = newLocation;
+        }
+        if (newStart != '') {
+            result.start = newStart;
+        }
+        if (newEnd != '') {
+            result.end = newEnd;
+        }
+        if (newHrs != '') {
+            result.hours = newHrs;
+        }
     },
-    // Explain what function D does
+    // This get all the objects in the array
     getTimetable() {
         return this.timetable;
     },
-    // Explain what function E does
+    // get all the same module name and add the number of hours
     getTotalHrs(moduleName) {
-        
+        sum = 0;
+        const result = this.timetable.filter(({ module }) => module === moduleName);
+        result.forEach(timetable => sum = sum + timetable.hours);
+        return sum;
     }
 }
